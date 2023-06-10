347b18937f40a89803134e6cd97db3d0608a696e
radifkhanrafin <meherafkabir@gmail.com>
2023-06-10 11:02:02 +0600

    student dashbord complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
@@ -10,7 +11,26 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://meherafkabir:7rMQPpezvXEmrGGX@summer-school.9wyvcpv.mongodb.net/?retryWrites=true&w=majority";
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    console.log('authorization', authorization)
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access 1' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+    // console.log('token', token)
+    jwt.verify(token, process.env.JWT_ACCESS_TOCKEN, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access 2' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@summer-school.9wyvcpv.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,13 +49,32 @@ async function run() {
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-        const usercollection = client.db("summer_school").collection("user");
+        const userscollection = client.db("summer_school").collection("users");
         const Instructorscollection = client.db("summer_school").collection("Instructors");
+        const SelectedCoursecollection = client.db("summer_school").collection("SelectedCourse");
 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            console.log('from user /jwt', user)
+            console.log(process.env.JWT_ACCESS_TOCKEN)
+            const token = jwt.sign(user, process.env.JWT_ACCESS_TOCKEN, { expiresIn: '20h' })
+            res.send({ token })
+        })
 
-
-        app.get('/user', async (req, res) => {
-            const result = await usercollection.find().toArray()
+        app.post('/users', async (req, res) => {
+            const users = req.body;
+            const query = { email: users.email }
+            const existingUser = await userscollection.findOne(query);
+            // console.log("existing", existingUser)
+            if (existingUser) {
+                console.log('ai user ase')
+                return res.send({ mssage: 'user Is already exists' })
+            }
+            const result = await userscollection.insertOne(users);
+            res.send(result)
+        })
+        app.get('/users', async (req, res) => {
+            const result = await userscollection.find().toArray()
             res.send(result)
         })
         app.get('/instructors', async (req, res) => {
@@ -43,9 +82,33 @@ async function run() {
             res.send(result)
         })
         app.get('/instructors/:id', async (req, res) => {
-            const id=req.params.id;
-            const query={_id : new ObjectId(id)}
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
             const result = await Instructorscollection.findOne(query)
+            res.send(result)
+        })
+
+
+        app.get('/SelectedCourse/:email', async (req, res) => {
+            const email = req.params.email;
+            // console.log(email)
+            const query = { email: email };
+            const result = await SelectedCoursecollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.post('/SelectCourse', async (req, res) => {
+            const course = req.body;
+            console.log('course', course)
+            const result = await SelectedCoursecollection.insertOne(course);
+            res.send(result)
+        })
+
+        app.delete('/SelectCourse/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('hit delete', id)
+            const query = { _id: new ObjectId(id) };
+            const result = await SelectedCoursecollection.deleteOne(query);
             res.send(result)
         })
 
